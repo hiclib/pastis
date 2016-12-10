d94bc3fb6e1f1d3a32323018b215845712c44ec5
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-12-09 16:47:37 -0800

    Sparse MDS now works fine

--- pastis/optimization/mds.py
+++ pastis/optimization/mds.py
@@ -5,14 +5,22 @@ from sklearn.utils import check_random_state
 from sklearn.metrics import euclidean_distances
 from sklearn.isotonic import IsotonicRegression
 
-from utils import compute_wish_distances
+from .utils import compute_wish_distances
 
 
 def MDS_obj(X, distances):
+    if sparse.issparse(distances):
+        return MDS_obj_sparse(X, distances)
+    else:
+        return MDS_obj_dense(X, distances)
+
+
+def MDS_obj_dense(X, distances):
     X = X.reshape(-1, 3)
     dis = euclidean_distances(X)
     X = X.flatten()
-    return ((dis - distances)**2).sum()
+    obj = 1. / distances ** 2 * (dis - distances) ** 2
+    return obj[np.invert(np.isnan(obj) | np.isinf(obj))].sum()
 
 
 def MDS_obj_sparse(X, distances):
@@ -22,13 +30,20 @@ def MDS_obj_sparse(X, distances):
 
 
 def MDS_gradient(X, distances):
+    if sparse.issparse(distances):
+        return MDS_gradient_sparse(X, distances)
+    else:
+        return MDS_gradient_dense(X, distances)
+
+
+def MDS_gradient_dense(X, distances):
     X = X.reshape(-1, 3)
     m, n = X.shape
     tmp = X.repeat(m, axis=0).reshape((m, m, n))
     dif = tmp - tmp.transpose(1, 0, 2)
     dis = euclidean_distances(X).repeat(3, axis=1).flatten()
     distances = distances.repeat(3, axis=1).flatten()
-    grad = dif.flatten() * (dis - distances) / dis / distances.data**2
+    grad = 2 * dif.flatten() * (dis - distances) / dis / distances**2
     grad[(distances == 0) | np.isnan(grad)] = 0
     X = X.flatten()
     return grad.reshape((m, m, n)).sum(axis=1).flatten()
@@ -38,8 +53,8 @@ def MDS_gradient_sparse(X, distances):
     X = X.reshape(-1, 3)
     dis = np.sqrt(((X[distances.row] - X[distances.col])**2).sum(axis=1))
 
-    grad = ((dis - distances.data) /
-            dis / distances.data**2)[:, np.newaxis] * (
+    grad = 2 * ((dis - distances.data) / dis /
+                distances.data**2)[:, np.newaxis] * (
         X[distances.row] - X[distances.col])
     grad_ = np.zeros(X.shape)
 
@@ -53,30 +68,21 @@ def MDS_gradient_sparse(X, distances):
 
 def estimate_X(counts, alpha=-3., beta=1., ini=None,
                verbose=0,
-               bias=None,
-               factr=1e12,
                use_zero_entries=False,
-               precompute_distances="auto",
                random_state=None, type="MDS2",
                maxiter=10000):
     n = counts.shape[0]
 
     random_state = check_random_state(random_state)
-    if ini is None or ini == "random":
+    if ini is None:
         ini = 1 - 2 * random_state.rand(n * 3)
 
-    if precompute_distances == "auto":
-        distances = compute_wish_distances(counts, alpha=alpha, beta=beta,
-                                           bias=bias)
-    elif precompute_distances == "precomputed":
-        distances = counts
-
+    distances = compute_wish_distances(counts, alpha=alpha, beta=beta)
     results = optimize.fmin_l_bfgs_b(
-        MDS_obj_sparse, ini.flatten(),
-        MDS_gradient_sparse,
+        MDS_obj, ini.flatten(),
+        MDS_gradient,
         (distances, ),
-        iprint=verbose,
-        factr=factr,
+        iprint=1,
         maxiter=maxiter)
     return results[0].reshape(-1, 3)
 
@@ -182,5 +188,5 @@ class NMDS(object):
                                bias=self.bias,
                                factr=self.factr,
                                maxiter=self.max_iter)
-        print "writing wish distances"
+        print("writing wish distances")
         return X
