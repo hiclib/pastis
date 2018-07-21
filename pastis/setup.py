fbb212c7dea11d07d5c826959595d2588ef2b033
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2018-07-20 17:42:04 -0700

    FIX installation, finally, maybe?

--- pastis/setup.py
+++ pastis/setup.py
@@ -1,12 +1,8 @@
 import os
-from os.path import join
-import warnings
 
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
-    from numpy.distutils.system_info import get_info, BlasNotFoundError
-    import numpy
 
     libraries = []
     if os.name == 'posix':
