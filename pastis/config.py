6a4eb1a630236b10853b61a66880586207dbd28f
Gesine Cauer <gesine.cauer@gmail.com>
2019-05-21 15:53:32 -0700

    added code to save input params for diploid pastis inference

--- pastis/config.py
+++ pastis/config.py
@@ -5,6 +5,71 @@ except ImportError:
 import os
 from distutils.util import strtobool
 
+import pandas as pd
+import numpy as np
+
+
+def save_params(counts, infer_lengths, alpha, beta, ploidy, init, outdir, lagrange_mult, constraints, homo_init, input_weight, as_sparse, norm, filter_counts,
+                infer_chrom, stepwise_genome=False, stepwise_genome__lowres_min_beads=5, stepwise_genome__fix_homo=True,
+                lowres_genome_factor=None, multiscale_rounds=None, HSC_lowres_beads=5, modifications=None, null=False, filter_percentage=0.04, max_iter=1e40, in_2d=False):
+    from topsy.inference.poisson_diploid import check_constraints
+
+    params = pd.Series({'counts': ','.join(sorted([{1: 'ambig', 1.5: 'pa', 2: 'ua'}[sum(c.shape) / (infer_lengths.sum() * ploidy)] for c in counts])),
+                        'ploidy': ploidy,
+                        'normalize': norm,
+                        'filter': (str(int(filter_percentage * 100)) + '%' if norm and filter_counts else None),
+                        'init': init,
+                        'alpha': alpha,
+                        'beta': (None if beta is None else ','.join([str(b) for b in beta])),
+                        'max_iter': max_iter,
+                        'homo_init': homo_init,
+                        'multiscale_rounds': multiscale_rounds,
+                        'lengths': ",".join(map(str, infer_lengths)),
+                        'chrom': ",".join(infer_chrom),
+                        'input_weight': (None if input_weight is None else ','.join([str(x) for x in input_weight])),
+                        'sparsity': ('sparse' if as_sparse else 'dense'),
+                        'null_inference': ('- Poisson' if null else '+ Poisson')})
+
+    if len(infer_lengths) > 1:
+        params['stepwise_genome'] = stepwise_genome
+        if stepwise_genome:
+            params['stepwise_genome.lowres_min_beads'] = stepwise_genome__lowres_min_beads
+            params['stepwise_genome.fix_homo'] = stepwise_genome__fix_homo
+
+    lagrange_mult, constraints = check_constraints(lagrange_mult, constraints, verbose=False)
+    for k, v in lagrange_mult.items():
+        if v is not None and v != 0:
+            params['constraint.%s' % k] = constraints[k]
+            params['lambda.%s' % k] = v
+            if k.lower() == 'homodis':
+                params['homodis.lowres_beads'] = HSC_lowres_beads
+
+    if 'in_2d':
+        params['in_2d'] = True
+    if modifications is not None and len(modifications) != 0:
+        params['modifications'] = '.'.join(modifications)
+
+    if multiscale_rounds is not None:
+        if lowres_genome_factor is not None:
+            raise ValueError("Must set either multiscale_rounds or lowres_genome_factor, not both")
+        all_multiscale_factors = 2 ** np.flip(np.arange(multiscale_rounds), axis=0)
+    elif lowres_genome_factor is not None:
+        all_multiscale_factors = [lowres_genome_factor]
+    else:
+        raise ValueError("Must set multiscale_rounds or lowres_genome_factor, both are currently None")
+
+    for multiscale_factor in all_multiscale_factors:
+        current_outdir = outdir
+        if multiscale_factor != 1:
+            current_outdir = os.path.join(current_outdir, 'res-x-%d' % multiscale_factor)
+        try:
+            os.makedirs(current_outdir)
+        except OSError:
+            pass
+        params['multiscale_factor'] = multiscale_factor
+        params.to_csv(os.path.join(current_outdir, 'inference_params.txt'), sep='\t', header=False)
+        params['multiscale_factor'] = None
+
 
 def get_default_options():
     """
