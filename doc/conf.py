a9faab9d54b55ef46c98e585d1e1ee954d67af95
Gesine Cauer <gesine.cauer@gmail.com>
2019-12-11 17:51:28 -0800

    doc bug fix?

--- doc/conf.py
+++ doc/conf.py
@@ -12,10 +12,11 @@
 
 import os
 import sys
-import subprocess
+#import subprocess
 
-subprocess.call('pip install numpydoc', shell=True)
-sys.path.insert(0, os.path.abspath('../..'))
+#subprocess.call('pip install numpydoc', shell=True)
+#sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
