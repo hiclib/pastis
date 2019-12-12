3fa169a26217d6b436d8f177ee424ec1af8dfda4
Gesine Cauer <gesine.cauer@gmail.com>
2019-12-11 17:39:47 -0800

    documentation bug fix?

--- doc/conf.py
+++ doc/conf.py
@@ -9,11 +9,13 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+import subprocess
+
+subprocess.call('pip install numpydoc', shell=True)
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
