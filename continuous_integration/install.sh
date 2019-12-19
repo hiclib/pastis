ad3877cacf2986bf33372574067b5f5a6d603530
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-12-19 14:35:18 +0100

    Updating travis-CI scripts

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -3,87 +3,76 @@
 # .travis.yml. See http://docs.travis-ci.com/ for more details.
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
-#
-# This script is adapted from a similar script from the scikit-learn repository.
-#
+
 # License: 3-clause BSD
 
+# Travis clone hiclib/iced repository in to a local repository.
+# We use a cached directory with three iced repositories (one for each
+# matrix entry) from which we pull from local Travis repository. This allows
+# us to keep build artefact for gcc + cython, and gain time
+
 set -e
+pip install --upgrade pip nose
 
-# Fix the compilers to workaround avoid having the Python 3.4 build
-# lookup for g++44 unexpectedly.
-export CC=gcc
-export CXX=g++
-
-print_conda_requirements() {
-    # Echo a conda requirement string for example
-    # "pip nose python='.7.3 scikit-learn=*". It has a hardcoded
-    # list of possible packages to install and looks at _VERSION
-    # environment variables to know whether to install a given package and
-    # if yes which version to install. For example:
-    #   - for numpy, NUMPY_VERSION is used
-    #   - for scikit-learn, SCIKIT_LEARN_VERSION is used
-    TO_INSTALL_ALWAYS="python=${PYTHON_VERSION} numpy=${NUMPY_VERSION} scipy=${SCIPY_VERSION} scikit-learn=${SKLEARN_VERSION} pandas=${PANDAS_VERSION} pip nose pytest pytest-cov cython"
-    REQUIREMENTS="$TO_INSTALL_ALWAYS"
-    TO_INSTALL_MAYBE="python numpy scipy scikit-learn pandas pip nose pytest pytest-coverage cython"
-    for PACKAGE in $TO_INSTALL_MAYBE; do
-        # Capitalize package name and add _VERSION
-        PACKAGE_VERSION_VARNAME="${PACKAGE^^}_VERSION"
-        # replace - by _, needed for scikit-learn for example
-        PACKAGE_VERSION_VARNAME="${PACKAGE_VERSION_VARNAME//-/_}"
-        # dereference $PACKAGE_VERSION_VARNAME to figure out the
-        # version to install
-        PACKAGE_VERSION="${!PACKAGE_VERSION_VARNAME}"
-        if [ -n "$PACKAGE_VERSION" ]; then
-            REQUIREMENTS="$REQUIREMENTS $PACKAGE=$PACKAGE_VERSION"
-        fi
-    done
-    echo $REQUIREMENTS
-}
-
-create_new_conda_env() {
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
-
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b
-    export PATH=/home/travis/miniconda2/bin:$PATH
-    conda update --yes conda
-
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    REQUIREMENTS=$(print_conda_requirements)
-    echo "conda requirements string: $REQUIREMENTS"
-    conda create -n testenv --yes $REQUIREMENTS
-    source activate testenv
-
-    if [[ "$INSTALL_MKL" == "true" ]]; then
-        # Make sure that MKL is used
-        conda install --yes mkl
-    else
-        # Make sure that MKL is not used
-        conda remove --yes --features mkl || echo "MKL not installed"
-    fi
-}
-
-if [[ "$DISTRIB" == "conda" ]]; then
-    create_new_conda_env
-    # Note: nibabel is in setup.py install_requires so nibabel will
-    # always be installed eventually. Defining NIBABEL_VERSION is only
-    # useful if you happen to want a specific nibabel version rather
-    # than the latest available one.
-    if [ -n "$NIBABEL_VERSION" ]; then
-        pip install nibabel=="$NIBABEL_VERSION"
-    fi
-
+if [[ $NUMPY_VERSION != "*" ]]; then
+    pip install --upgrade \
+        numpy==$NUMPY_VERSION
 else
-    echo "Unrecognized distribution ($DISTRIB); cannot setup travis environment."
-    exit 1
+    pip install numpy --upgrade
 fi
+
+if [[ $SCIPY_VERSION != "*" ]]; then
+    pip install --upgrade scipy==$SCIPY_VERSION
+else
+    pip install --upgrade scipy
+fi
+
+
+if [[ $PANDAS_VERSION != "*" ]]; then
+    pip install --upgrade pandas==$PANDAS_VERSION
+else
+    pip install --upgrade pandas
+fi
+
+if [[ $SKLEARN_VERSION != "*" ]]; then
+    pip install --upgrade scikit-learn==$SKLEARN_VERSION
+else
+    pip install --upgrade scikit-learn
+fi
+
+
+pip install cython
+
+
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install pytest-cov pytest coverage coveralls
+fi
+
+if [ ! -d "$CACHED_BUILD_DIR" ]; then
+    mkdir -p $CACHED_BUILD_DIR
+fi
+
+rsync -av --exclude '.git/' --exclude='testvenv/' \
+      $TRAVIS_BUILD_DIR $CACHED_BUILD_DIR
+
+cd $CACHED_BUILD_DIR/pastis
+
+# Build pastis in the install.sh script to collapse the verbose
+# build output in the travis output when it succeeds.
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+make cython
+python setup.py build_src
+python setup.py develop
+
+if [[ "$RUN_FLAKE8" == "true" ]]; then
+    # flake8 version is temporarily set to 2.5.1 because the next
+    # version available on conda (3.3.0) has a bug that checks non
+    # python files and cause non meaningful flake8 errors
+    pip install flake8==2.5.1
+fi
+
 
 pip install coverage coveralls pytest-coverage
 pip install iced
