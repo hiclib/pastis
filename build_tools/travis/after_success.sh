a5662af8e7d92960834d35b3b16b69c5d9c96191
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-01-15 15:31:56 +0100

    Updating versions

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -5,7 +5,8 @@
 # License: 3-clause BSD
   
 set -e
-    
+set -o xtrace
+   
 if [[ "$COVERAGE" == "true" ]]; then
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
