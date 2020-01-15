59751468b4aba6c42b0e3c77ab84d40a74122e5a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-01-15 15:54:37 +0100

    MAINT codecov now works (#62)

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -7,11 +7,12 @@
 set -e
 set -o xtrace
    
-if [[ "$COVERAGE" == "true" ]]; then
+if [[ "$COVERAGE" == true ]]; then
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
     pip install codecov
-    cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
+    #pushd $TEST_DIR
+    #cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
 
-    codecov --root $TRAVIS_BUILD_DIR || echo "codecov upload failed"
+    codecov --root $TEST_DIR || echo "codecov upload failed"
 fi
