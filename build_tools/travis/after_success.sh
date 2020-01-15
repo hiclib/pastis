78d8c788561189776995ef8943561dc0b02d0fe6
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-01-15 15:40:10 +0100

    Uploading codecov

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -7,7 +7,7 @@
 set -e
 set -o xtrace
    
-if [[ "$COVERAGE" == "true" ]]; then
+if [[ "$COVERAGE" == true ]]; then
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
     pip install codecov
