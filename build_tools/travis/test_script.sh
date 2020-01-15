209efcee2ba6fb8954ec312b9ef1da17c27b077c
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-01-15 13:44:56 +0100

    MAINT cleaning up installation scripts for travis-ci

---
+++ build_tools/travis/test_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+set -e
+set -v
+
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
+
+run_tests() {
+    TEST_CMD="pytest --showlocals --pyargs"
+
+    # Get into a temp directory to run test from the installed scikit learn
+    # and
+    # check if we do not leave artifacts
+    mkdir -p $TEST_DIR
+    pushd $TEST_DIR
+
+    if [[ "$COVERAGE" == "true" ]]; then
+        TEST_CMD="$TEST_CMD --cov pastis"
+    fi
+    $TEST_CMD pastis
+    popd
+    
+}
+
+if [[ "$RUN_FLAKE8" == "true" ]]; then
+    source build_tools/travis/flake8_diff.sh
+fi
+
+if [[ "$SKIP_TESTS" != "true" ]]; then
+    run_tests
+fi
+
