a91f75e20d09e0cf8d5b135a3636f26b6d6ea3e2
Gesine Cauer <gesine.cauer@gmail.com>
2020-01-22 12:36:50 -0800

    added full test of algorithm with unambig

---
+++ pastis/optimization/tests/test_pastis_algorithms.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.metrics import euclidean_distances
+from numpy.testing import assert_array_almost_equal
+from scipy import sparse
+from pastis.optimization import pastis_algorithms
+
+
+def test_pastis_poisson_unambig():
+    lengths = np.array([25])
+    ploidy = 2
+    seed = 42
+    bcc_lambda = 0
+    hsc_lambda = 0
+    hsc_r = 0
+    alpha, beta = -3., 1.
+
+    random_state = np.random.RandomState(seed=seed)
+    X_true = random_state.rand(lengths.sum() * ploidy, 3)
+    dis = euclidean_distances(X_true)
+    dis[dis == 0] = np.inf
+    counts = beta * dis ** alpha
+    counts[np.isnan(counts) | np.isinf(counts)] = 0
+    counts = np.triu(counts, 1)
+    counts = sparse.coo_matrix(counts)
+
+    struct_, infer_var = pastis_algorithms.pastis_poisson(
+        counts, lengths, ploidy, outdir=None, alpha=alpha, seed=seed,
+        normalize=False, filter_threshold=0, bcc_lambda=bcc_lambda,
+        hsc_lambda=hsc_lambda, hsc_r=hsc_r, print_freq=None, history_freq=None,
+        save_freq=None)
