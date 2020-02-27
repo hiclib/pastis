fb9c0ac243c74067631fa16bcd224e713c63f30a
Gesine Cauer <gesine.cauer@gmail.com>
2020-02-27 10:55:34 -0800

    added io module

--- pastis/setup.py
+++ pastis/setup.py
@@ -13,6 +13,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage("optimization")
     config.add_subpackage("externals")
     config.add_subpackage("datasets")
+    config.add_subpackage("io")
     return config
 
 
